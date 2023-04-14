9001de17141d470e70a55d53560aa101677525a6
wocjs602 <wocjs602@gmail.com>
2023-04-14 14:55:42 +0900

    make_models.py s

--- 01_skeleton/movies/models.py
+++ 01_skeleton/movies/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+from django.conf import settings
 
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
-
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
     def __str__(self):
-        return self.title
+        return f'{self.id}번째글 - {self.title}'
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
